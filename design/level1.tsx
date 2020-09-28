9aa2f5d9d904140051516b031263e4051b2568e5
Mihai Vasilache <mihai.vasilache@edenred.com>
2020-09-28 14:41:58 +0300

    add graphics src

---
+++ design/level1.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="level1" tilewidth="48" tileheight="48" tilecount="52" columns="13">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="phaser/src/assets/tiles/tiles.png" width="662" height="204"/>
+</tileset>
