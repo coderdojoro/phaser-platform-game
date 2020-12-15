c74be9522d76996e1201b6f13c4c32017739db8a
Mihai Vasilache <mihai.vasilache@edenred.com>
2020-12-15 19:26:42 +0200

    log

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var fileServer = new static.Server('./dist');
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
+        console.log('serve: ' + request);
         fileServer.serve(request, response);
     }).resume();
 }).listen(80);
